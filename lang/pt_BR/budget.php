48d47f233ffa793e60b0c0cd2cd509d5f2a05da7
Max Carvalho <45166618+maxacarvalho@users.noreply.github.com>
2023-04-05 22:45:09 +0100

    Updates budget translated name (#17)

--- lang/pt_BR/budget.php
+++ lang/pt_BR/budget.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'budget' => 'orçamento',
-    'budgets' => 'orçamentos',
+    'budget' => 'pedido de compra',
+    'budgets' => 'pedidos de compra',
     'id' => 'ID',
     'company_id' => 'empresa',
     'company' => 'empresa',
     'company_code' => 'filial',
     'company_code_branch' => 'código da filial',
-    'budget_number' => 'número orçamento',
+    'budget_number' => 'número pedido de compra',
     'status' => 'status',
     'created_at' => 'criado em',
     'updated_at' => 'atualizado em',
