b6b4806ffbeaaf951f45babffdd902d6a7f406e8
Max Carvalho <maaxxicarvalho@gmail.com>
2023-05-12 17:26:34 +0100

    Installs Microsoft Clarity

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::shouldBeStrict();
 
+        if ($this->app->environment('production')) {
+            Filament::registerScripts([
+                asset('js/clarity.js'),
+            ]);
+        }
+
         Filament::registerScripts([
             'https://cdn.jsdelivr.net/npm/@alpinejs/mask@3.x.x/dist/cdn.min.js',
         ], true);
