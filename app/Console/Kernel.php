daab7d8c339c624c5c972813e541dec34a89e359
Max Carvalho <maaxxicarvalho@gmail.com>
2023-05-15 15:22:42 +0100

    Enables command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\ProcessBuyerInvitationsCommand;
+use App\Console\Commands\ProcessSupplierInvitationsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -11,6 +12,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command(ProcessBuyerInvitationsCommand::class)
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->onOneServer();
+
+        $schedule->command(ProcessSupplierInvitationsCommand::class)
             ->everyMinute()
             ->withoutOverlapping()
             ->onOneServer();
