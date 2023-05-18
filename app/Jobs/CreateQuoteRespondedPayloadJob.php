2144c6bde3bbc063cc4fb01d9ba170c5f17612c7
Max Carvalho <maaxxicarvalho@gmail.com>
2023-05-18 14:36:36 +0100

    Logs quotes without buyer for investigation

--- app/Jobs/CreateQuoteRespondedPayloadJob.php
+++ app/Jobs/CreateQuoteRespondedPayloadJob.php
@@ -6,12 +6,15 @@ use App\Data\Protheus\Quote\Out\ProtheusQuotePayloadData;
 use App\Enums\PayloadProcessingStatusEnum;
 use App\Models\IntegrationType;
 use App\Models\Payload;
+use App\Models\Product;
 use App\Models\Quote;
+use App\Models\QuoteItem;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class CreateQuoteRespondedPayloadJob implements ShouldQueue
 {
@@ -26,10 +29,26 @@ class CreateQuoteRespondedPayloadJob implements ShouldQueue
     {
         /** @var Quote $quote */
         $quote = Quote::query()
-            ->with('items', 'items.product', 'items.product.company')
+            ->with(
+                Quote::RELATION_COMPANY,
+                Quote::RELATION_SUPPLIER,
+                Quote::RELATION_PAYMENT_CONDITION,
+                Quote::RELATION_BUYER,
+                Quote::RELATION_ITEMS,
+                Quote::RELATION_ITEMS.'.'.QuoteItem::RELATION_PRODUCT,
+                Quote::RELATION_ITEMS.'.'.QuoteItem::RELATION_PRODUCT.'.'.Product::RELATION_COMPANY
+            )
             ->findOrFail($this->quoteId);
 
         if (! $quote->isResponded()) {
+            $this->delete();
+        }
+
+        if (null === $quote->buyer) {
+            Log::warning('Quote without buyer', [
+                'quote_id' => $quote->id,
+            ]);
+
             $this->delete();
         }
 
