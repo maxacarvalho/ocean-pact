f9b522c2439f4bcc8bf543a2424d0f4523e8f40e
Max Carvalho <maaxxicarvalho@gmail.com>
2023-05-19 11:00:57 +0100

    Fixes type error

--- app/Data/Protheus/Quote/In/ProtheusQuoteItemPayloadData.php
+++ app/Data/Protheus/Quote/In/ProtheusQuoteItemPayloadData.php
@@ -5,6 +5,7 @@ namespace App\Data\Protheus\Quote\In;
 use App\Data\Protheus\Quote\ProtheusProductPayloadData;
 use App\Models\QuoteItem;
 use Brick\Money\Money;
+use NumberFormatter;
 use Spatie\LaravelData\Data;
 
 class ProtheusQuoteItemPayloadData extends Data
@@ -14,7 +15,7 @@ class ProtheusQuoteItemPayloadData extends Data
         public string $UNIDADE_MEDIDA,
         public string $ITEM,
         public float $QUANTIDADE,
-        public float $PRECO_UNITARIO,
+        public string $PRECO_UNITARIO,
         public ?string $OBS,
         public ProtheusProductPayloadData $PRODUTO
     ) {
@@ -27,9 +28,19 @@ class ProtheusQuoteItemPayloadData extends Data
             UNIDADE_MEDIDA: $quote->measurement_unit,
             ITEM: $quote->item,
             QUANTIDADE: $quote->quantity,
-            PRECO_UNITARIO: Money::ofMinor($quote->unit_price, 'BRL')->formatTo('pt_BR'),
+            PRECO_UNITARIO: Money::ofMinor($quote->unit_price, 'BRL')->formatWith(self::getFormatter()),
             OBS: $quote->comments,
             PRODUTO: ProtheusProductPayloadData::fromQuoteItem($quote)
         );
+    }
+
+    private static function getFormatter(): NumberFormatter
+    {
+        $formatter = new NumberFormatter('pt_BR', NumberFormatter::DECIMAL);
+        $formatter->setSymbol(NumberFormatter::DECIMAL_SEPARATOR_SYMBOL, ',');
+        $formatter->setSymbol(NumberFormatter::GROUPING_SEPARATOR_SYMBOL, '.');
+        $formatter->setAttribute(NumberFormatter::FRACTION_DIGITS, 2);
+
+        return $formatter;
     }
 }
