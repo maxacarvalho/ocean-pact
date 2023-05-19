3aa6b5c1f14101cd72613d67e1dae9a9b1f2c0ce
Max Carvalho <maaxxicarvalho@gmail.com>
2023-05-19 10:23:17 +0100

    Updates the supplier login/register logic

--- config/filament.php
+++ config/filament.php
@@ -87,7 +87,7 @@ return [
     'auth' => [
         'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
         'pages' => [
-            'login' => \JeffGreco13\FilamentBreezy\Http\Livewire\Auth\Login::class,
+            'login' => null, // \JeffGreco13\FilamentBreezy\Http\Livewire\Auth\Login::class,
         ],
     ],
 
