028f6370b5a68049e24efc3a8278641111b79474
Max Carvalho <maaxxicarvalho@gmail.com>
2023-08-25 15:22:40 +0100

    Installs Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,6 +11,8 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('telescope:prune')->daily();
+
         $schedule->command(ProcessBuyerInvitationsCommand::class)
             ->everyMinute()
             ->withoutOverlapping()
