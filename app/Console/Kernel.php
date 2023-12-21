04999a683ad6b305f456f93a0b842bd518e51742
Max Carvalho <maaxxicarvalho@gmail.com>
2023-12-21 16:00:25 +0000

    Removes Laravel Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('telescope:prune')->daily();
+        //
     }
 
     protected function commands(): void
