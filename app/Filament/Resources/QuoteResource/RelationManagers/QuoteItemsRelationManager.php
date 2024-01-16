41118f2ab73f9bce12524132927e8c8f4a664195
caiofcosta <caiocostasp@gmail.com>
2024-01-16 13:38:15 -0500

    filtros parcial fornecedores

--- app/Filament/Resources/QuoteResource/RelationManagers/QuoteItemsRelationManager.php
+++ app/Filament/Resources/QuoteResource/RelationManagers/QuoteItemsRelationManager.php
@@ -354,21 +354,21 @@ class QuoteItemsRelationManager extends RelationManager
             ->actions([
                 TableEditAction::make()
                     ->using(function (QuoteItem $record, array $data) {
-                        if (
-                            null !== $record->seller_image
-                            &&
-                            (isset($data[QuoteItem::SELLER_IMAGE]) && (null === $data[QuoteItem::SELLER_IMAGE] || $record->seller_image !== $data[QuoteItem::SELLER_IMAGE]))
-                        ) {
-                            Storage::disk('s3')->delete($record->seller_image);
-                        }
+                        // if (
+                        //     null !== $record->seller_image
+                        //     &&
+                        //     (isset($data[QuoteItem::SELLER_IMAGE]) && (null === $data[QuoteItem::SELLER_IMAGE] || $record->seller_image !== $data[QuoteItem::SELLER_IMAGE]))
+                        // ) {
+                        //     Storage::disk('s3')->delete($record->seller_image);
+                        // }
 
-                        if (
-                            null !== $record->buyer_image
-                            &&
-                            (isset($data[QuoteItem::BUYER_IMAGE]) && (null === $data[QuoteItem::BUYER_IMAGE] || $record->buyer_image !== $data[QuoteItem::BUYER_IMAGE]))
-                        ) {
-                            Storage::disk('s3')->delete($record->buyer_image);
-                        }
+                        // if (
+                        //     null !== $record->buyer_image
+                        //     &&
+                        //     (isset($data[QuoteItem::BUYER_IMAGE]) && (null === $data[QuoteItem::BUYER_IMAGE] || $record->buyer_image !== $data[QuoteItem::BUYER_IMAGE]))
+                        // ) {
+                        //     Storage::disk('s3')->delete($record->buyer_image);
+                        // }
 
                         $record->update($data);
 
