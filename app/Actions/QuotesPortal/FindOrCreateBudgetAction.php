119d6c60ff8f69a2a6535c26a0605fc1e32d33b6
Max Carvalho <maaxxicarvalho@gmail.com>
2024-02-09 00:12:18 +0000

    Upgrade PHP 8.3 and possible fix for product duplication

--- app/Actions/QuotesPortal/FindOrCreateBudgetAction.php
+++ app/Actions/QuotesPortal/FindOrCreateBudgetAction.php
@@ -10,12 +10,21 @@ class FindOrCreateBudgetAction
 {
     public function handle(QuoteData $data, Company $company): Budget
     {
-        /** @var Budget $budget */
-        $budget = Budget::query()->firstOrCreate([
-            Budget::COMPANY_CODE => $company->code,
-            Budget::COMPANY_CODE_BRANCH => $company->code_branch,
-            Budget::BUDGET_NUMBER => $data->budget->budget_number,
-        ]);
+        /** @var Budget|null $budget */
+        $budget = Budget::query()
+            ->where(Budget::COMPANY_CODE, '=', $company->code)
+            ->where(Budget::COMPANY_CODE_BRANCH, '=', $company->code_branch)
+            ->where(Budget::BUDGET_NUMBER, '=', $data->budget->budget_number)
+            ->first();
+
+        if (null === $budget) {
+            /** @var Budget $budget */
+            $budget = Budget::query()->create([
+                Budget::COMPANY_CODE => $company->code,
+                Budget::COMPANY_CODE_BRANCH => $company->code_branch,
+                Budget::BUDGET_NUMBER => $data->budget->budget_number,
+            ]);
+        }
 
         return $budget;
     }
