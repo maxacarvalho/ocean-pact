e2476a2df743f66a0f78165025419d9ae12e2f09
Gabriel Baierle <gbaierle92@gmail.com>
2024-02-26 23:41:47 -0300

    fixes integration_type tests to avoid database connection issues

--- tests/Unit/Models/IntegrationTypeTest.php
+++ tests/Unit/Models/IntegrationTypeTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\IntegraHub\IntegrationType;
 use Illuminate\Support\Carbon;
+use Mockery;
 
 describe('Scheduling interval is due method', function () {
     test('should be false when interval is not set', function () {
@@ -22,16 +23,18 @@ describe('Scheduling interval is due method', function () {
     });
 
     test('should be false when last_run_at diff in minutes is lower than interval', function () {
-        $integrationType = new IntegrationType();
-        $integrationType->interval = 10;
-        $integrationType->last_run_at = Carbon::now()->subMinutes(5);
+        $integrationType = Mockery::mock(IntegrationType::class, function ($mock) {
+            $mock->shouldReceive('getAttribute')->with('interval')->andReturn(10);
+            $mock->shouldReceive('getAttribute')->with('last_run_at')->andReturn(Carbon::now()->subMinutes(5));
+        })->makePartial();
         expect($integrationType->isDue())->toBeFalse();
     });
 
     test('should be true when last_run_at diff in minutes is greater than interval', function () {
-        $integrationType = new IntegrationType();
-        $integrationType->interval = 10;
-        $integrationType->last_run_at = Carbon::now()->subMinutes(20);
+        $integrationType = Mockery::mock(IntegrationType::class, function ($mock) {
+            $mock->shouldReceive('getAttribute')->with('interval')->andReturn(10);
+            $mock->shouldReceive('getAttribute')->with('last_run_at')->andReturn(Carbon::now()->subMinutes(20));
+        })->makePartial();
         expect($integrationType->isDue())->toBeTrue();
     });
 });
