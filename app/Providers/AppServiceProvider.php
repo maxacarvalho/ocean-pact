ea449a593a486de25217e18ea0922a749d65f562
Shift <shift@laravelshift.com>
2024-03-15 10:43:44 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,28 @@
 namespace App\Providers;
 
 use App\Livewire\Synth\MoneySynth;
+use App\Models\User;
 use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentView;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     public function register(): void
     {
     }
@@ -44,5 +55,16 @@ class AppServiceProvider extends ServiceProvider
             'panels::user-menu.before',
             fn (): string => Blade::render('<livewire:locale-switcher/>'),
         );
+
+        $this->bootAuth();
+    }
+
+    public function bootAuth(): void
+    {
+        $this->registerPolicies();
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isSuperAdmin();
+        });
     }
 }
