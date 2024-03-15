0872b18e9bdb355f546cd22194b76a91fb31d77c
Shift <shift@laravelshift.com>
2024-03-15 10:43:45 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,26 +7,6 @@ return [
 
     'currency' => 'BRL',
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'domain' => env('FILAMENT_DOMAIN'),
 
